20ead9caec6c4d8d9a3b188c10b3d74cf01d14aa
Kari Marttila <kari.marttila@iki.fi>
2019-01-25 21:10:42 +0200

    Some modifications, not yet working

---
+++ simple-server-vm/packer/create-tar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SS_DIR=/mnt/edata/aw/kari/github/clojure/clj-ring-cljs-reagent-demo/simple-server
+
+# TODO: Here also make the build first for the right profile.
+
+rm -rf ./tmp
+mkdir -p tmp/logs
+cp $SS_DIR/target/uberjar/simple-server-1.0-standalone.jar tmp/app.jar
+cp -r $SS_DIR/resources tmp/.
+cp install/start-server.sh tmp/.
+cd tmp
+tar -zcvf ../app.tar.gz *
+cd ..
+rm app.tar.gz
+
